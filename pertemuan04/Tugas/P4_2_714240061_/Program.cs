340c5fb47c753360d8d6136a3325b844cbbb5e04
Zidanhairraramadhan <zidanhairra@gmail.com>
2025-11-08 22:56:17 +0700

    v

---
+++ pertemuan04/Tugas/P4_2_714240061_/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_2_714240061_
+{
+    public class Mahasiswa
+    {
+        // Field private
+        private string _nama;
+        private string _kelas;
+        private string _npm;
+
+        // Property public untuk akses yang terkontrol (Encapsulation)
+        public string Nama
+        {
+            get { return _nama; }
+            // Set private agar hanya bisa diubah melalui constructor atau method di dalam class
+            private set { _nama = value; }
+        }
+
+        public string Kelas
+        {
+            get { return _kelas; }
+            private set { _kelas = value; }
+        }
+
+        public string Npm
+        {
+            get { return _npm; }
+            private set { _npm = value; }
+        }
+
+        // Constructor (Menginisialisasi objek saat dibuat)
+        public Mahasiswa(string nama, string kelas, string npm)
+        {
+            // Menggunakan set method dari Property
+            Nama = nama;
+            Kelas = kelas;
+            Npm = npm;
+        }
+
+        // Method untuk menampilkan data mahasiswa
+        public void TampilkanData()
+        {
+            Console.WriteLine($"- Nama: {Nama}");
+            Console.WriteLine($"  Kelas: {Kelas}");
+            Console.WriteLine($"  NPM: {Npm}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Data Mahasiswa Kelas 2C ===");
+            Console.WriteLine("-------------------------------\n");
+
+            // Membuat objek Mahasiswa (Menggunakan Constructor)
+            Mahasiswa mhs1 = new Mahasiswa("Zidan Hairra Ramadhan", "2C", "714240061");
+            Mahasiswa mhs2 = new Mahasiswa("Malik", "2C", "714240062");
+            Mahasiswa mhs3 = new Mahasiswa("Bagus Tri Atmaza", "2C", "714240060");
+
+            // Menggunakan List untuk menyimpan dan mengelola data
+            List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa> { mhs1, mhs2, mhs3 };
+
+            // Menampilkan output untuk setiap mahasiswa
+            foreach (var mhs in daftarMahasiswa)
+            {
+                mhs.TampilkanData();
+                Console.WriteLine(); // Baris kosong antar mahasiswa
+            }
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Mahasiswa Kelas 2C : {daftarMahasiswa.Count} Mahasiswa.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pertemuan04/Tugas/P4_2_714240061_/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("P4_2_714240061_")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("P4_2_714240061_")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("577f04be-bf96-457c-9207-3a0c45331484")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
