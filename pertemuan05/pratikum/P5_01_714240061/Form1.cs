e0960ff6b631ef1f7925b4686988e770c80ebc9c
Zidanhairraramadhan <zidanhairra@gmail.com>
2025-11-10 20:32:46 +0700

    v

---
+++ pertemuan05/pratikum/P5_01_714240061/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_01_714240061
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloButton_Click(object sender, EventArgs e)
+        {
+            OutputLabel.Text = "Hello World";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
