1be03cfdcfdd8b314baa4e795956d9b873695ba6
Zidanhairraramadhan <zidanhairra@gmail.com>
2025-11-10 23:08:35 +0700

    v

---
+++ pertemuan05/pratikum/P5_3_714240061/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace P5_3_714240061
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMerkHP = new System.Windows.Forms.TextBox();
+            this.rb_android = new System.Windows.Forms.RadioButton();
+            this.cbYa = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rb_ios = new System.Windows.Forms.RadioButton();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMerkHP
+            // 
+            this.txtMerkHP.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMerkHP.Location = new System.Drawing.Point(186, 32);
+            this.txtMerkHP.Name = "txtMerkHP";
+            this.txtMerkHP.Size = new System.Drawing.Size(216, 31);
+            this.txtMerkHP.TabIndex = 0;
+            // 
+            // rb_android
+            // 
+            this.rb_android.AutoSize = true;
+            this.rb_android.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_android.Location = new System.Drawing.Point(186, 89);
+            this.rb_android.Name = "rb_android";
+            this.rb_android.Size = new System.Drawing.Size(112, 30);
+            this.rb_android.TabIndex = 1;
+            this.rb_android.TabStop = true;
+            this.rb_android.Text = "Android";
+            this.rb_android.UseVisualStyleBackColor = true;
+            // 
+            // cbYa
+            // 
+            this.cbYa.AutoSize = true;
+            this.cbYa.Location = new System.Drawing.Point(243, 142);
+            this.cbYa.Name = "cbYa";
+            this.cbYa.Size = new System.Drawing.Size(55, 24);
+            this.cbYa.TabIndex = 2;
+            this.cbYa.Text = "Ya";
+            this.cbYa.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Merk HP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "OS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sudah Diperbaiki?";
+            // 
+            // rb_ios
+            // 
+            this.rb_ios.AutoSize = true;
+            this.rb_ios.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_ios.Location = new System.Drawing.Point(320, 93);
+            this.rb_ios.Name = "rb_ios";
+            this.rb_ios.Size = new System.Drawing.Size(67, 30);
+            this.rb_ios.TabIndex = 6;
+            this.rb_ios.TabStop = true;
+            this.rb_ios.Text = "iOS";
+            this.rb_ios.UseVisualStyleBackColor = true;
+            // 
+            // btnTampilkan
+            // 
+            this.btnTampilkan.Location = new System.Drawing.Point(85, 206);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(75, 23);
+            this.btnTampilkan.TabIndex = 7;
+            this.btnTampilkan.Text = "Tampilkan";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
+            this.btnTampilkan.Click += new System.EventHandler(this.btnTampilkan_Click);
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.Location = new System.Drawing.Point(269, 206);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(75, 23);
+            this.btnKeluar.TabIndex = 8;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(444, 259);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnTampilkan);
+            this.Controls.Add(this.rb_ios);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbYa);
+            this.Controls.Add(this.rb_android);
+            this.Controls.Add(this.txtMerkHP);
+            this.Name = "Form1";
+            this.Text = "Service HP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMerkHP;
+        private System.Windows.Forms.RadioButton rb_android;
+        private System.Windows.Forms.CheckBox cbYa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rb_ios;
+        private System.Windows.Forms.Button btnTampilkan;
+        private System.Windows.Forms.Button btnKeluar;
+    }
+}
+
