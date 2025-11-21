99137d6c0ad19d0959cf748e095cdb8b410061a7
Zidanhairraramadhan <zidanhairra@gmail.com>
2025-11-22 00:02:57 +0700

    v

---
+++ pertemuan06/P6_4_714240061/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace P6_4_714240061
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNumeric = new System.Windows.Forms.TextBox();
+            this.txtChar = new System.Windows.Forms.TextBox();
+            this.txtRequired = new System.Windows.Forms.TextBox();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.txtCompare1 = new System.Windows.Forms.TextBox();
+            this.txtCompare2 = new System.Windows.Forms.TextBox();
+            this.txtUpper = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtLower = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNumeric
+            // 
+            this.txtNumeric.Location = new System.Drawing.Point(189, 117);
+            this.txtNumeric.MaxLength = 20;
+            this.txtNumeric.Name = "txtNumeric";
+            this.txtNumeric.Size = new System.Drawing.Size(100, 26);
+            this.txtNumeric.TabIndex = 0;
+            this.txtNumeric.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumeric_KeyPress);
+            // 
+            // txtChar
+            // 
+            this.txtChar.Location = new System.Drawing.Point(189, 73);
+            this.txtChar.MaxLength = 20;
+            this.txtChar.Name = "txtChar";
+            this.txtChar.Size = new System.Drawing.Size(100, 26);
+            this.txtChar.TabIndex = 1;
+            this.txtChar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtChar_KeyPress);
+            // 
+            // txtRequired
+            // 
+            this.txtRequired.Location = new System.Drawing.Point(189, 211);
+            this.txtRequired.Name = "txtRequired";
+            this.txtRequired.Size = new System.Drawing.Size(100, 26);
+            this.txtRequired.TabIndex = 2;
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Location = new System.Drawing.Point(189, 165);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(100, 26);
+            this.txtRegex.TabIndex = 4;
+            // 
+            // txtCompare1
+            // 
+            this.txtCompare1.Location = new System.Drawing.Point(618, 79);
+            this.txtCompare1.Name = "txtCompare1";
+            this.txtCompare1.Size = new System.Drawing.Size(100, 26);
+            this.txtCompare1.TabIndex = 6;
+            // 
+            // txtCompare2
+            // 
+            this.txtCompare2.Location = new System.Drawing.Point(618, 123);
+            this.txtCompare2.Name = "txtCompare2";
+            this.txtCompare2.Size = new System.Drawing.Size(100, 26);
+            this.txtCompare2.TabIndex = 8;
+            // 
+            // txtUpper
+            // 
+            this.txtUpper.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtUpper.Location = new System.Drawing.Point(618, 220);
+            this.txtUpper.MaxLength = 10;
+            this.txtUpper.Name = "txtUpper";
+            this.txtUpper.Size = new System.Drawing.Size(100, 26);
+            this.txtUpper.TabIndex = 10;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(618, 171);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 26);
+            this.txtLength.TabIndex = 12;
+            // 
+            // txtLower
+            // 
+            this.txtLower.BackColor = System.Drawing.Color.White;
+            this.txtLower.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtLower.ForeColor = System.Drawing.Color.Black;
+            this.txtLower.Location = new System.Drawing.Point(385, 265);
+            this.txtLower.Name = "txtLower";
+            this.txtLower.Size = new System.Drawing.Size(100, 26);
+            this.txtLower.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(45, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "ID Anggota";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(45, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(45, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Alamat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(45, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 20);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(424, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Black;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(424, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 20);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Konfirmasi Password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Black;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(424, 177);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 20);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Kode Rahasia";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Black;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(424, 220);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 20);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Username ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Black;
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(254, 268);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 20);
+            this.label9.TabIndex = 30;
+            this.label9.Text = "Pesan";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(298, 323);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 33);
+            this.btnSubmit.TabIndex = 31;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Goudy Stout", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(67, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(611, 30);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "New Anggota Naga Hitam";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLower);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtUpper);
+            this.Controls.Add(this.txtCompare2);
+            this.Controls.Add(this.txtCompare1);
+            this.Controls.Add(this.txtRegex);
+            this.Controls.Add(this.txtRequired);
+            this.Controls.Add(this.txtChar);
+            this.Controls.Add(this.txtNumeric);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pendaftaran Naga hitam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumeric;
+        private System.Windows.Forms.TextBox txtChar;
+        private System.Windows.Forms.TextBox txtRequired;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.TextBox txtCompare1;
+        private System.Windows.Forms.TextBox txtCompare2;
+        private System.Windows.Forms.TextBox txtUpper;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtLower;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ pertemuan06/P6_4_714240061/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace P6_4_714240061
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void ClearAllErrors(Control control)
+        {
+            epWarning.SetError(control, "");
+            epWrong.SetError(control, "");
+            epCorrect.SetError(control, "");
+        }
+
+        
+
+        // A. NUMERIC VALIDATOR (Event KeyPress)
+        private void txtNumeric_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // B. CHAR VALIDATOR (Event KeyPress)
+        private void txtChar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // Form_Load (Untuk penataan, seperti yang sudah kita buat)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // ... (Kode penataan Form1_Load yang rapi diletakkan di sini) ...
+            // Jika Anda sudah menaruhnya di Designer, Anda bisa mengosongkan ini.
+        }
+
+       
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // --- 1. Bersihkan semua ErrorProvider sebelum memulai pengecekan ---
+            ClearAllErrors(txtNumeric);
+            ClearAllErrors(txtChar);
+            ClearAllErrors(txtRequired);
+            ClearAllErrors(txtRegex);
+            ClearAllErrors(txtCompare1);
+            ClearAllErrors(txtCompare2);
+            ClearAllErrors(txtLength);
+            ClearAllErrors(txtUpper);
+            ClearAllErrors(txtLower);
+
+            bool isFormValid = true;
+
+           
+            if (this.txtNumeric.Text.Length != 5) // Contoh Length: harus 5 digit
+            {
+                epWrong.SetError(this.txtNumeric, "ID Anggota harus 5 digit!");
+                isFormValid = false;
+            }
+            // Karena KeyPress sudah membatasi input, kita asumsikan jika panjang benar, inputnya valid
+            else
+            {
+                epCorrect.SetError(this.txtNumeric, "ID sudah benar.");
+            }
+
+            if (string.IsNullOrWhiteSpace(this.txtChar.Text))
+            {
+                epWrong.SetError(this.txtChar, "Nama wajib diisi.");
+                isFormValid = false;
+            }
+            else
+            {
+                epCorrect.SetError(this.txtChar, "Nama sudah valid.");
+            }
+
+            
+            if (string.IsNullOrWhiteSpace(this.txtRequired.Text))
+            {
+                epWrong.SetError(this.txtRequired, "Alamat wajib diisi!");
+                isFormValid = false;
+            }
+            else
+            {
+                epCorrect.SetError(this.txtRequired, "Alamat sudah terisi.");
+            }
+
+            
+            if (!Regex.IsMatch(this.txtRegex.Text, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                epWrong.SetError(this.txtRegex, "Format Email tidak valid!");
+                isFormValid = false;
+            }
+            else if (this.txtRegex.Text.EndsWith(".co") || this.txtRegex.Text.EndsWith(".biz"))
+            {
+                // Contoh penggunaan epWarning
+                epWarning.SetError(this.txtRegex, "Domain non-standar terdeteksi.");
+            }
+            else
+            {
+                epCorrect.SetError(this.txtRegex, "Email valid.");
+            }
+
+            
+            if (string.IsNullOrWhiteSpace(this.txtCompare1.Text) || string.IsNullOrWhiteSpace(this.txtCompare2.Text))
+            {
+                epWrong.SetError(this.txtCompare1, "Password wajib diisi!");
+                epWrong.SetError(this.txtCompare2, "Konfirmasi wajib diisi!");
+                isFormValid = false;
+            }
+            else if (this.txtCompare1.Text != this.txtCompare2.Text)
+            {
+                epWrong.SetError(this.txtCompare1, "Password tidak cocok!");
+                epWrong.SetError(this.txtCompare2, "Password tidak cocok!");
+                isFormValid = false;
+            }
+            else
+            {
+                epCorrect.SetError(this.txtCompare1, "Password cocok.");
+                epCorrect.SetError(this.txtCompare2, "Password cocok.");
+            }
+
+           
+            if (this.txtLength.Text.Length < 5 || this.txtLength.Text.Length > 10)
+            {
+                epWrong.SetError(this.txtLength, "Panjang harus 5-10 karakter!");
+                isFormValid = false;
+            }
+            else
+            {
+                epCorrect.SetError(this.txtLength, "Panjang sesuai.");
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.txtUpper.Text))
+                epCorrect.SetError(this.txtUpper, "Otomatis Upper Case.");
+
+            if (!string.IsNullOrWhiteSpace(this.txtLower.Text))
+                epCorrect.SetError(this.txtLower, "Otomatis Lower Case.");
+
+           
+            if (isFormValid)
+            {
+                string hasil =
+                    "--- HASIL ISIAN FORM ---" + "\n" +
+                    "ID Anggota (a, f) : " + this.txtNumeric.Text + "\n" +
+                    "Nama (b, g)       : " + this.txtChar.Text + "\n" +
+                    "Alamat (c)        : " + this.txtRequired.Text + "\n" +
+                    "Email (d)         : " + this.txtRegex.Text + "\n" +
+                    "Password (e)      : ******" + "\n" +
+                    "Kode Promo (f)    : " + this.txtLength.Text + "\n" +
+                    "Nama Ibu (g)      : " + this.txtUpper.Text + "\n" +
+                    "Catatan (h)       : " + this.txtLower.Text;
+
+                MessageBox.Show(hasil, "HASIL VALIDASI SUKSES");
+            }
+            else
+            {
+                MessageBox.Show("Terdapat kesalahan dalam pengisian form. Silakan periksa ikon merah.", "Validasi Gagal");
+            }
+        }
+    }
+}
