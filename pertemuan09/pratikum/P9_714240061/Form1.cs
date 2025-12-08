fc4670fa3602c4e196720d9a6a4cf17e315a3953
Zidanhairraramadhan <zidanhairra@gmail.com>
2025-12-08 14:53:40 +0700

    v

---
+++ pertemuan09/pratikum/P9_714240061/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P9_714240061
+{
+
+    public partial class Form1 : Form
+    {
+        Koneksi koneksi = new Koneksi();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Tampil()
+        {
+            // Query SELECT untuk mengambil semua data mahasiswa
+            string query = "SELECT * FROM t_mahasiswa";
+
+            // Memanggil method ShowData() dari class Koneksi
+            DataMahasiswa.DataSource = koneksi.ShowData(query);
+
+            // Mengubah nama header kolom (opsional, tetapi dianjurkan)
+            DataMahasiswa.Columns[0].HeaderText = "NPM";
+            DataMahasiswa.Columns[1].HeaderText = "Nama";
+            DataMahasiswa.Columns[2].HeaderText = "Angkatan";
+            DataMahasiswa.Columns[3].HeaderText = "Alamat";
+            DataMahasiswa.Columns[4].HeaderText = "Email";
+            DataMahasiswa.Columns[5].HeaderText = "No HP";
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Tampil();
+        }
+    }
+}
---
+++ pertemuan09/pratikum/P9_714240061/Koneksi.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P9_714240061
+{
+        internal class Koneksi
+    {
+        string connectionstring = "Server=localhost;Database=pemrog2ulbi;Uid=root;Pwd=;";
+        public MySqlConnection koneksi;
+
+        public void OpenConnection()
+        {
+            koneksi = new MySqlConnection(connectionstring);
+            koneksi.Open();
+        }
+
+        public void CloseConnection()
+        {
+            koneksi.Close();
+        }
+
+        public object ShowData(string query)
+        {
+            MySqlDataAdapter data = new MySqlDataAdapter(query, connectionstring);
+
+            DataSet DS = new DataSet();
+            data.Fill(DS);
+            return DS.Tables[0];
+        }
+    }
+}
+
+
+
